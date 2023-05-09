d261f9b0d391087639e74e2f37473133d5882e04
gg8765407 <132796077+gg8765407@users.noreply.github.com>
2023-05-09 21:01:27 +0800

    script.js

--- fei.js
+++ fei.js
@@ -1,143 +1,88 @@
-const canvas = document.getElementById("myCanvas");
-const ctx = canvas.getContext("2d");
-
-const images = [];
-let currentImageIndex = 0;
-
-for (let i = 1; i <= 219; i++) {
-    const img = new Image();
-    img.src = "png/a (" + i + ").png";
-    images.push(img);
-}
-
-function draw() {
-    ctx.clearRect(0, 0, canvas.width, canvas.height);
-    const scale = Math.min(canvas.width / images[currentImageIndex].width, canvas.height / images[currentImageIndex].height);
-    const width = images[currentImageIndex].width * scale;
-    const height = images[currentImageIndex].height * scale;
-    const x = (canvas.width - width) / 2;
-    const y = (canvas.height - height) / 2;
-    ctx.drawImage(images[currentImageIndex], x, y, width, height);
-}
-
-function changeImage(index) {
-    currentImageIndex = index;
-    draw();
-}
-
-document.addEventListener("keydown", (event) => {
-    switch (event.keyCode) {
-        case 37:
-            if (currentImageIndex > 0) {
-                changeImage(currentImageIndex - 1);
-            }
-            break;
-        case 39:
-            if (currentImageIndex < images.length - 1) {
-                changeImage(currentImageIndex + 1);
-            }
-            break;
+<!DOCTYPE html>
+<html>
+<head>
+  <meta charset="utf-8">
+  <title>Canvas Demo</title>
+  <style>
+    canvas {
+      border: 1px solid black;
+      margin-right: 10px;
     }
-});
+  </style>
+</head>
+<body>
+  <canvas id="myCanvas1"></canvas>
+  <canvas id="myCanvas2"></canvas>
 
-images[0].onload = () => {
-    canvas.width = window.innerWidth;
-    canvas.height = window.innerHeight;
-    setInterval(() => {
-        if (currentImageIndex < images.length - 1) {
-            currentImageIndex++;
-        } else {
-            currentImageIndex = 0;
+  <script>
+    // 获取canvas元素和上下文对象
+    const canvas1 = document.getElementById("myCanvas1");
+    const ctx1 = canvas1.getContext("2d");
+
+    const canvas2 = document.getElementById("myCanvas2");
+    const ctx2 = canvas2.getContext("2d");
+
+    // 图片数组和当前显示的图片下标
+    const images = [];
+    let currentImageIndex = 0;
+
+    // 加载所有图片
+    for (let i = 1; i <= 219; i++) {
+        const img = new Image();
+        img.src = "png/a (" + i + ").png";
+        images.push(img);
+    }
+
+    // 绘制当前图片到canvas上
+    function draw1() {
+        ctx1.clearRect(0, 0, canvas1.width, canvas1.height);
+        ctx1.drawImage(images[currentImageIndex], 0, 0, canvas1.width, canvas1.height);
+    }
+
+    function draw2() {
+        ctx2.clearRect(0, 0, canvas2.width, canvas2.height);
+        ctx2.drawImage(images[currentImageIndex], 0, 0, canvas2.width, canvas2.height);
+    }
+
+    // 切换图片的函数
+    function changeImage(index) {
+        currentImageIndex = index;
+        draw1();
+        draw2();
+    }
+
+    // 添加键盘事件监听，左箭头键切换到上一张图片，右箭头键切换到下一张图片
+    document.addEventListener("keydown", (event) => {
+        switch (event.keyCode) {
+            case 37: // 左箭头键
+                if (currentImageIndex > 0) {
+                    changeImage(currentImageIndex - 1);
+                }
+                break;
+            case 39: // 右箭头键
+                if (currentImageIndex < images.length - 1) {
+                    changeImage(currentImageIndex + 1);
+                }
+                break;
         }
-        draw();
-    }, 50);
-};
+    });
 
-// 添加第二个canvas元素
-const canvas2 = document.getElementById("myCanvas2");
-const ctx2 = canvas2.getContext("2d");
-
-const circles = [];
-const circleRadius = 50;
-const maxSpeed = 5;
-const minSpeed = 1;
-const circleCount = 30;
-
-for (let i = 0; i < circleCount; i++) {
-    const circle = {
-        x: Math.random() * canvas2.width,
-        y: Math.random() * canvas2.height,
-        vx: Math.random() * (maxSpeed - minSpeed) + minSpeed,
-        vy: Math.random() * (maxSpeed - minSpeed) + minSpeed,
-        color: getRandomColor(),
+    // 加载第一张图片并添加动画效果
+    images[0].onload = () => {
+        canvas1.width = images[0].width;
+        canvas1.height = images[0].height;
+        canvas2.width = images[0].width;
+        canvas2.height = images[0].height;
+        setInterval(() => {
+            if (currentImageIndex < images.length - 1) {
+                currentImageIndex++;
+            } else {
+                currentImageIndex = 0;
+            }
+            draw1();
+            draw2();
+        }, 50);
     };
-    circles.push(circle);
-}
-
-function getRandomColor() {
-    const colors = ["#FF6138", "#FFFF9D", "#BEEB9
-function drawCircles() {
-ctx2.clearRect(0, 0, canvas2.width, canvas2.height);
-for (let i = 0; i < circles.length; i++) {
-const circle = circles[i];
-circle.x += circle.vx;
-circle.y += circle.vy;
-if (circle.x > canvas2.width + circleRadius) {
-circle.x = -circleRadius;
-}
-if (circle.y > canvas2.height + circleRadius) {
-circle.y = -circleRadius;
-}
-if (circle.x < -circleRadius) {
-circle.x = canvas2.width + circleRadius;
-}
-if (circle.y < -circleRadius) {
-circle.y = canvas2.height + circleRadius;
-}
-ctx2.beginPath();
-ctx2.arc(circle.x, circle.y, circleRadius, 0, 2 * Math.PI);
-ctx2.fillStyle = circle.color;
-ctx2.fill();
-}
-}
-
-function animate() {
-drawCircles();
-requestAnimationFrame(animate);
-}
-
-animate();
-function drawCircles() {
-ctx2.clearRect(0, 0, canvas2.width, canvas2.height);
-circles.forEach((circle) => {
-ctx2.beginPath();
-ctx2.arc(circle.x, circle.y, circleRadius, 0, 2 * Math.PI);
-ctx2.fillStyle = circle.color;
-ctx2.fill();
-});
-}
-
-function updateCircles() {
-circles.forEach((circle) => {
-circle.x += circle.vx;
-circle.y += circle.vy;
-    if (circle.x < circleRadius || circle.x > canvas2.width - circleRadius) {
-        circle.vx *= -1;
-    }
-
-    if (circle.y < circleRadius || circle.y > canvas2.height - circleRadius) {
-        circle.vy *= -1;
-    }
-});
-}
-
-function animate() {
-requestAnimationFrame(animate);
-updateCircles();
-drawCircles();
-}
-
-canvas2.width = window.innerWidth;
-canvas2.height = window.innerHeight;
-
-animate();
+  </script>
+</body>
+</html>
