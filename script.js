65551033ee1bd2329cbf8a02c7fdc1129f4ad077
gg8765407 <132796077+gg8765407@users.noreply.github.com>
2023-05-09 22:13:45 +0800

    script.js

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-// 获取canvas元素和上下文对象
+// 获取 canvas 元素和上下文对象
 const canvas = document.getElementById("myCanvas");
 const ctx = canvas.getContext("2d");
 
@@ -7,57 +7,61 @@ const images = [];
 let currentImageIndex = 0;
 
 // 加载所有图片
-for (let i = 1; i <= 219; i++) {
-  const img = new Image();
-  img.src = "png/a (" + i + ").png";
-  images.push(img);
+for (let i = 1; i <= 2; i++) {
+    const img = new Image();
+    img.src = `png/${i}.jpg`;
+    images.push(img);
 }
 
-// 绘制当前图片到canvas上
+// 绘制当前图片到 canvas 上
 function draw() {
-  // 将canvas尺寸调整为全屏
-  canvas.width = window.innerWidth;
-  canvas.height = window.innerHeight;
-
-  ctx.clearRect(0, 0, canvas.width, canvas.height);
-  // 将当前图片按比例扩大到全屏
-  ctx.drawImage(images[currentImageIndex], 0, 0, canvas.width, canvas.width * (images[currentImageIndex].height / images[currentImageIndex].width));
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+    if (canvas.width >= canvas.height) {
+        ctx.drawImage(images[currentImageIndex], 0, 0, canvas.width / 2, canvas.height);
+        ctx.drawImage(images[currentImageIndex + 1], canvas.width / 2, 0, canvas.width / 2, canvas.height);
+    } else {
+        ctx.drawImage(images[currentImageIndex], 0, 0, canvas.width, canvas.height / 2);
+        ctx.drawImage(images[currentImageIndex + 1], 0, canvas.height / 2, canvas.width, canvas.height / 2);
+    }
 }
 
 // 切换图片的函数
 function changeImage(index) {
-  currentImageIndex = index;
-  draw();
+    currentImageIndex = index;
+    draw();
 }
 
 // 添加键盘事件监听，左箭头键切换到上一张图片，右箭头键切换到下一张图片
 document.addEventListener("keydown", (event) => {
-  switch (event.keyCode) {
-    case 37: // 左箭头键
-      if (currentImageIndex > 0) {
-        changeImage(currentImageIndex - 1);
-      }
-      break;
-    case 39: // 右箭头键
-      if (currentImageIndex < images.length - 1) {
-        changeImage(currentImageIndex + 1);
-      }
-      break;
-  }
+    switch (event.keyCode) {
+        case 37: // 左箭头键
+            if (currentImageIndex > 0) {
+                changeImage(currentImageIndex - 2);
+            }
+            break;
+        case 39: // 右箭头键
+            if (currentImageIndex < images.length - 2) {
+                changeImage(currentImageIndex + 2);
+            }
+            break;
+    }
 });
 
 // 加载第一张图片并添加动画效果
 images[0].onload = () => {
-  // 将canvas尺寸调整为全屏
-  canvas.width = window.innerWidth;
-  canvas.height = window.innerHeight;
-
-  setInterval(() => {
-    if (currentImageIndex < images.length - 1) {
-      currentImageIndex++;
+    if (canvas.width >= canvas.height) {
+        canvas.width = images[0].width * 2;
+        canvas.height = images[0].height;
     } else {
-      currentImageIndex = 0;
+        canvas.width = images[0].width;
+        canvas.height = images[0].height * 2;
     }
-    draw();
-  }, 50);
+    setInterval(() => {
+        if (currentImageIndex < images.length - 2) {
+            currentImageIndex += 2;
+        } else {
+            currentImageIndex = 0;
+        }
+        draw();
+    }, 50);
 };
